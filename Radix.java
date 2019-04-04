5ce9092ed97cf47b85993a22c4ac28e8cdd6887a
ElizaHlglh <ewu4@stuy.edu>
2019-04-03 22:52:00 -0400

    Radix.java

--- Radix.java
+++ Radix.java
@@ -14,13 +14,21 @@ public class Radix{
     for (int exp = 1; exp <= maxUnit; exp++){
       //MyLinkedList[] budgets = new MyLinkedList[20];//individual budges of each unit
       for (int i = 0; i < data.length; i++){//at 10^maxUnit, the largest value in this will have
-        int digit = (data[i]%(Math.pow(10, exp)))/(Math.pow(10, exp-1));
+        int digit = (data[i]% (int) (Math.pow(10, exp)))/( (int) Math.pow(10, exp-1));
         if (data[i] < 0){//if it is a negative number
           budget[9-digit].add(data[i]);
         }
         else{
           budget[digit+10].add(data[i]);
         }
+      }
+      MyLinkedList result = new MyLinkedList(); //merge all list in the budget into a whole list
+      for (int k = 0; k < budget.length; k++){
+        result.extend(budget[k]);
+      }
+      //edit data to equal to the list
+      for (int l = 0; l < data.length; l++){
+        data[l] = result.get(l);
       }
     }
   }
